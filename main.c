6117e074a7a0d65fc7e9765dd3d58453817598a1
naveen billapati <billapatinaveen143@gmail.com>
2020-05-31 03:09:04 -0700

    added user menu function to main.c

--- main.c
+++ main.c
@@ -10,21 +10,27 @@
 	#define CLEAR system("clear");	//If linux/Mac
 #endif
 
-int menu(){ //Menu options, returns the selected option
+int userMenu(){ // displays User Menu, then returns the selected option
 	int option;
 
 	printf("\t[1] Add Todays Record \n");
 	printf("\t[2] View Previous Records\n");
 	printf("\t[2] Edit Records\n");
+	printf("\t[2] Delete Records\n");
+	printf("\t[2] Search Records \n");
+	printf("\t[2] Monthly CalenderView \n");
+	printf("\t[2] Edit Password \n"); // this option will be available in release 2
 
-	printf("Choice : ");
+	printf("Please Enter your Choice : ");
 
 	option = scanf("%d", &option);
 
 	return option;
 }
 
-
+void welcome_screen(){
+	
+}
 
 int main()
 {
@@ -32,11 +38,12 @@ int main()
 	
 	
 		CLEAR
-		
+		welcome_screen(); // show welcome page
+
 		printf("********   Welcome To your Secured Virtual Diary   ********\n");
 
 	
-		printf(" Please Select an option from menu :)\n");
+		printf(" Please Select an option from the User menu\n");
 		int option = menu();
 printf(" You Have Selected option %d \n",option);
 
