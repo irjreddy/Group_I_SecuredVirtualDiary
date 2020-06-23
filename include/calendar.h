dc6a299b04888bc920ddf8fc5eff73f790dd2ded
naveenbillapati <56974000+naveenbillapati@users.noreply.github.com>
2020-06-23 00:26:15 -0700

    fixed git merge comments

--- include/calendar.h
+++ include/calendar.h
@@ -3,10 +3,10 @@
 #include<windows.h>
 #include<time.h>
 
-<<<<<<< HEAD
+
 int runCalendar();
 void displayCalendar(int,int,int,int[]);
 void calendar(int,int);
-=======
+
 int runCalendar(user *loginptr);
->>>>>>> 1abe7438b2b30ce75ba571f220876dd82fe39bb9
+
